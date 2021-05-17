0127a4a7435fc233b57674839434a3f44a1fd2bc
arrudagates <arrudagates@gmail.com>
2021-05-17 16:42:30 -0300

    Full implementation of the cargo ligen command

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # cargo-ligen
 
 Cargo command for [LIGEN (Language Interface GENerator)](https://github.com/sensorial-systems/ligen)
+
+## Installation
+```
+cargo install --path .
+```
--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,38 @@
 use cargo_toml::Manifest;
-use std::{env, fs::copy, path::Path, process::Command};
-
-use indicatif::{ProgressBar, ProgressStyle};
+use std::{
+    env,
+    fs::copy,
+    path::Path,
+    process::{Command, Stdio},
+};
 
 fn main() {
+    let mut args = vec![String::from("+nightly"), String::from("build")];
+    let env_args: Vec<String> = env::args().collect();
+    args.extend_from_slice(env_args.get(2..env_args.len()).unwrap());
     if Path::new("./Cargo.toml").exists() {
-        env::args().into_iter().for_each(|arg| {
-            if let "--clean" = arg.as_str() {
-                let pb = ProgressBar::new_spinner();
-                pb.enable_steady_tick(120);
-                pb.set_style(
-                    ProgressStyle::default_spinner()
-                        .tick_strings(&[
-                            "▹▹▹▹▹",
-                            "▸▹▹▹▹",
-                            "▹▸▹▹▹",
-                            "▹▹▸▹▹",
-                            "▹▹▹▸▹",
-                            "▹▹▹▹▸",
-                            "▪▪▪▪▪",
-                        ])
-                        .template("{spinner:.blue} {msg}"),
-                );
-                pb.set_message("Cleaning...");
-                
-                // FIXME: Why is this needed?
-                let out = Command::new("cargo").arg("clean").output();
-
-                if let Ok(_) = out {
-                    pb.finish_with_message("Done");
-                } else {
-                    panic!("Failed to run cargo clean")
-                }
-            }
-        });
-
-        //TODO: reuse the progressbar code
-
-        let pb = ProgressBar::new_spinner();
-        pb.enable_steady_tick(120);
-        pb.set_style(
-            ProgressStyle::default_spinner()
-                .tick_strings(&[
-                    "▹▹▹▹▹",
-                    "▸▹▹▹▹",
-                    "▹▸▹▹▹",
-                    "▹▹▸▹▹",
-                    "▹▹▹▸▹",
-                    "▹▹▹▹▸",
-                    "▪▪▪▪▪",
-                ])
-                .template("{spinner:.blue} {msg}"),
-        );
-        pb.set_message("Building...");
-
-        let output = Command::new("cargo").arg("build").output();
+        let output = Command::new("cargo")
+            .args(args.as_slice())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .output();
 
         if let Ok(_) = output {
-            pb.finish_with_message("Done");
+            let name = Manifest::from_path("./Cargo.toml")
+                .expect("Failed to parse Cargo.toml")
+                .package
+                .expect("Failed to parse package section from Cargo.toml")
+                .name;
+
+            copy(
+                format!("./target/debug/lib{}.a", name),
+                format!("./target/ligen/{0}/lib/lib{0}.a", name),
+            )
+            .expect("Failed to copy lib");
         } else {
-            panic!("Failed to run cargo build")
+            panic!("Current directory is not a Cargo project");
         }
-
-        //TODO: Handle output
-
-        let name = Manifest::from_path("./Cargo.toml")
-            .expect("Failed to parse Cargo.toml")
-            .package
-            .expect("Failed to parse package section from Cargo.toml")
-            .name;
-
-        copy(
-            format!("./target/debug/lib{}.a", name),
-            format!("./target/ligen/{0}/lib/lib{0}.a", name),
-        )
-        .expect("Failed to copy lib");
     } else {
-        panic!("Current directory is not a Cargo project");
+        panic!("Failed to run cargo build");
     }
 }
-
-//TODO: Create tests
